321e174182cc9ffbde8970d9a56857ddd26cbdc3
vinicius <vinicius2011castro@hotmail.com>
2021-06-28 23:08:29 -0300

    Desafio Campo minado

---
+++ Ivory.TesteEstagio.CampoMinado/IdentificaMinas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ivory.TesteEstagio.CampoMinado
+{
+    class IdentificaMinas
+    {
+        int linha, coluna;
+
+        public IdentificaMinas (int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+        public int Linha
+        {
+            get { return linha; }
+            set { linha = value; }
+        }
+        public int Coluna
+        {
+            get { return coluna; }
+            set { coluna = value; }
+        }
+    }
+}
--- Ivory.TesteEstagio.CampoMinado/Program.cs
+++ Ivory.TesteEstagio.CampoMinado/Program.cs
@@ -4,6 +4,9 @@ namespace Ivory.TesteEstagio.CampoMinado
 {
     class Program
     {
+        public static int cont;
+        const int tamanho = 81;
+        public static IdentificaMinas[] posiValidas = new IdentificaMinas[tamanho];
         static void Main(string[] args)
         {
             var campoMinado = new CampoMinado();
@@ -11,7 +14,60 @@ namespace Ivory.TesteEstagio.CampoMinado
             Console.WriteLine(campoMinado.Tabuleiro);
 
             // Realize sua codificação a partir deste ponto, boa sorte!
+            IdentificaMinas minasV;
+            int i = 0;
+            while (campoMinado.JogoStatus ==0) 
+             {
+                 Console.WriteLine("Insira a linha onde voce quer fazer o seu movimento");
+                 int line = int.Parse(Console.ReadLine());
+                 Console.WriteLine("Insira a Coluna onde voce quer fazer o seu movimento");
+                 int column = int.Parse(Console.ReadLine());
 
+                campoMinado.Abrir(line, column);
+                verificaTabuleiro(line, column);
+
+                if (campoMinado.JogoStatus == 0)
+                 {
+                    Console.WriteLine("Proxima jogada");
+                    minasV = posiValidas[i];
+                    campoMinado.Abrir(minasV.Linha, minasV.Coluna);
+                    i++;
+                    Console.WriteLine(campoMinado.Tabuleiro);
+
+                }
+                 else if (campoMinado.JogoStatus == 1)
+                 {
+                     Console.WriteLine("Parabens voce consguiu passou com sucesso pelo campo minado");
+                     Console.WriteLine(campoMinado.Tabuleiro);
+                 }
+                 else 
+                 {
+                     Console.WriteLine("Game Over, voce encontrou uma bonba, mas não desanime da proxima voce consegue");
+                     Console.WriteLine(campoMinado.Tabuleiro);
+                 }
+             }
+            
+            Console.ReadKey();
+
+        }
+
+        static void verificaTabuleiro(int line ,int column) 
+        {
+            var campoMinado = new CampoMinado();
+            IdentificaMinas minasV;
+
+            if (campoMinado.JogoStatus == 0)
+            {
+                minasV = new IdentificaMinas(line + 1, column);
+                posiValidas[cont] = minasV;
+                cont++;
+            }
+            else if (campoMinado.JogoStatus == 2)
+            {
+                minasV = new IdentificaMinas(line, column + 1);
+                posiValidas[cont] = minasV;
+                cont++;
+            }
         }
     }
 }
